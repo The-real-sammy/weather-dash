56d2cae0c5a51e9ba110b6ecc0600bec29c0fe2d
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-03-15 15:30:15 +0000

    api key and urls added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// API KEY
+var apiKey = '8993ea18706be745ed1d2327a688cc19'
+
+//API URLS
+var geoApiURL = 'https://api.openweathermap.org/geo/1.0/direct?';
+var currentWeatherApiURL = 'https://api.openweathermap.org/data/2.5/weather?';
+var forecastWeatherApiURL = 'https://api.openweathermap.org/data/2.5/forecast?';
+
+//Weather Icon URL
+var weatherApiImage = 'https://openweathermap.org/img/wn/';
+
 // * Create a weather dashboard with form inputs.
+
+var searchForm = document.getElementById("search-form");// This is the Search form element 
+
+
 //   * When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+
 //   * When a user views the current weather conditions for that city they are presented with:
 //     * The city name
 //     * The date
@@ -7,6 +23,7 @@
 //     * The temperature
 //     * The humidity
 //     * The wind speed
+
 //   * When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
 //     * The date
 //     * An icon representation of weather conditions
