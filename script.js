1988c67da37ceabc947a48c6e873ab2ac9f7b2a2
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-03-15 15:48:48 +0000

    function to fetch cityname

--- script.js
+++ script.js
@@ -15,6 +15,46 @@ var searchForm = document.getElementById("search-form");// This is the Search fo
 
 
 //   * When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+function fetchWeatherData(cityName) {
+  // Fetch data from API
+  // Handle response and errors
+
+  var cityURL =
+  geoApiURL +
+  currentWeatherApiURL +
+    "&appid=8993ea18706be745ed1d2327a688cc19";
+
+  fetch(cityURL)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(cityURL);
+      console.log(data);
+
+      var lon = data[0].lon;
+      var lat = data[0].lat;
+
+      var longLatURL =
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+
+        "&appid=ef4ac2c82d48b8c9e99b208c1d96371c";
+      fetch(longLatURL)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          console.log(longLatURL);
+          console.log(data);
+        })
+        .catch(error => { // Handle any errors
+
+          console.error(error);
+})
+};
 
 //   * When a user views the current weather conditions for that city they are presented with:
 //     * The city name
